17573d57aa62a649cb2e39d505d3180711b4bf64
João Henrique <johnrick.jh@gmail.com>
2021-06-29 00:56:15 -0300

    Corrigindo problema no nome do service

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const elecService = require('./services/eleicaoService.js');
+const elecService = require('./services/eleicaoservice.js');
 
 var ocupado = false;
 
